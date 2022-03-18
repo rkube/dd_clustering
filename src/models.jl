56f7c4e30a68412fb93ffd9213e738f9b6eed449
Ralph Arthur Kube <rkube@stellar-intel.princeton.edu>
2022-03-18 14:36:49 -0400

    Improved debugging for circles

--- src/models.jl
+++ src/models.jl
@@ -6,6 +6,6 @@ using Flux
 function get_simple_mlp(hidden_dim::Int, num_classes::Int)
     Chain(Dense(2, hidden_dim, tanh),
           Dense(hidden_dim, hidden_dim, tanh),
-          Dense(hidden_dim, 8, tanh),
-          Parallel(vcat, x -> x, Chain(Dense(8, num_classes), x -> softmax(x))))
+          Dense(hidden_dim, hidden_dim, tanh),
+          Parallel(vcat, x -> x, Chain(Dense(hidden_dim, num_classes), x -> softmax(x))))
 end
--- src/train_circles.jl
+++ src/train_circles.jl
@@ -9,18 +9,18 @@ using Random
 using dd_clustering
 
 
-hidden_dim = 32
+hidden_dim = 128
 num_classes = 2
 
-r1 = 1.0
-r2 = 3.0
-r3 = 4.0
-num_pts = 10_000;
+r1 = 0.5
+r2 = 1.0
+r3 = 2.0
+num_pts = 10000;
 
 num_epochs = 20
 
-batch_size = 256;
-test_split = 0.8;
+batch_size = 1024;
+test_split = 0.5;
 
 # Get the dataset
 X, Y = gen_circles(r1, r2, r3, num_pts);
@@ -28,7 +28,7 @@ X, Y = gen_circles(r1, r2, r3, num_pts);
 # Scale X
 X = 2.0 * (X .- minimum(X)) / (maximum(X) - minimum(X)) .- 1.0;
 
-num_test = Int(num_pts * test_split);
+num_test = Int(round(num_pts * test_split));
 all_idx = shuffle(1:num_pts);
 train_idx = all_idx[1:num_test];
 test_idx = all_idx[num_test + 1:end];
@@ -36,10 +36,30 @@ test_idx = all_idx[num_test + 1:end];
 loader_train = DataLoader((data=X[:, train_idx], label=Y[:, train_idx]), batchsize=batch_size, shuffle=true);
 loader_test = DataLoader((data=X[:, test_idx], label=Y[:, test_idx]), batchsize=batch_size, shuffle=true);
 
-model = get_simple_mlp(hidden_dim, num_classes)
+model = Chain(Dense(2, hidden_dim, swish, init=Flux.kaiming_uniform),
+              Dense(hidden_dim, hidden_dim, swish, init=Flux.kaiming_uniform),
+              Dense(hidden_dim, hidden_dim, swish, init=Flux.kaiming_uniform),
+              Parallel(vcat, x -> x, Chain(Dense(hidden_dim, num_classes, init=Flux.kaiming_uniform), x -> softmax(x))))
 ps = Flux.params(model)
 
-opt = RMSProp(1e-3)
+opt = ADAM(1e-5)
+
+xy_grid = hcat(repeat(-1.0:0.01:1.0, outer=201), repeat(-1.0:0.01:1.0, inner=201))';
+y_pred = model(xy_grid)[end-1:end,:];
+label1 = y_pred[1,:] .> y_pred[2,:];
+p = plot(xy_grid[1, label1], xy_grid[2, label1], title="epoch $(epoch)", seriestype=:scatter)
+plot!(p, xy_grid[1, .!(label1)], xy_grid[2, .!(label1)], seriestype=:scatter)
+savefig(p, "decisionboundary_epoch0.png");
+x_t, y_t = first(loader_test);
+y_pred = model(x_t)[end-1:end, :]
+label1 = y_pred[1,:] .> y_pred[2,:]
+p = plot(x_t[1, label1], x_t[2, label1], title="epoch $(epoch)", seriestype=:scatter)
+plot!(p, x_t[1, .!(label1)], x_t[2, .!(label1)], seriestype=:scatter)
+savefig(p, "points_epoch$(epoch).png")
+
+p = plot(y_pred[1, label1], y_pred[2, label1], title="epoch $(epoch)", seriestype=:scatter)
+plot!(p, y_pred[1, .!(label1)], y_pred[2, .!(label1)], title="epoch $(epoch)", seriestype=:scatter)
+savefig(p, "cluster_assignment$(epoch).png")
 
 for epoch in 1:num_epochs
     for (x,y) in loader_train
@@ -51,24 +71,24 @@ for epoch in 1:num_epochs
             A = y_pred[end-1:end, :]            # A contains the cluster assignments. Compared to the article, A is transposed
 
             distances = reshape([norm(y_hidden[:, i] - y_hidden[:, j]) for i ∈ 1:batch_size, j ∈ 1:batch_size], (batch_size, batch_size))
-            sigma2 = (0.15 * mean(distances))^2
+            sigma2 = 0.15f0 * mean(distances)
 
-            K = exp.(-0.5 .* distances .* distances / sigma2)
+            K = exp.(-0.5f0 .* distances .* distances / sigma2)
             M = reshape([exp(-norm(A[:, q] - ((1:num_classes) .== i))^2) for i ∈ 1:num_classes, q ∈ 1:batch_size], (num_classes, batch_size))
 
-            loss_cs = 0.0
-            loss_orth = 0.0
+            loss_cs = 0.0f0
+            loss_orth = 0.0f0
             for i ∈ 1:(num_classes - 1)
                 for j ∈ (i + 1):num_classes
-                    loss_cs += A[i, :]' * K * A[j, :] / sqrt(A[i, :]' * K * A[i, :] * A[j, :]' * K * A[j, :])
-                    loss_orth += M[i, :]' * K * M[j, :] / sqrt(M[i, :]' * K * M[i, :] * M[j, :]' * K * M[j, :])
+                    loss_cs += A[i, :]' * K * A[j, :] / sqrt(A[i, :]' * K * A[i, :] * A[j, :]' * K * A[j, :] + eps(eltype(K)))
+                    loss_orth += M[i, :]' * K * M[j, :] / sqrt(M[i, :]' * K * M[i, :] * M[j, :]' * K * M[j, :] + eps(eltype(K)))
                 end
             end
 
-            tmp = A * A';
+            tmp = A' * A;
             loss_triu = sum([i > j ? tmp[i, j] : 0.0 for i ∈ 1:num_classes, j ∈ 1:num_classes])
 
-            # @show loss_cs, loss_orth, loss_triu
+            @show loss_cs, loss_orth, loss_triu, sigma2
             
             (loss_cs + loss_orth) / num_classes + loss_triu
         end
@@ -76,11 +96,23 @@ for epoch in 1:num_epochs
 
         Flux.update!(opt, ps, grads)
     end
+    xy_grid = hcat(repeat(-1.0:0.01:1.0, outer=201), repeat(-1.0:0.01:1.0, inner=201))';
+    y_pred = model(xy_grid)[end-1:end,:]
+    label1 = y_pred[1,:] .> y_pred[2,:]
+    p = plot(xy_grid[1, label1], xy_grid[2, label1], title="epoch $(epoch)", seriestype=:scatter)
+    plot!(p, xy_grid[1, .!(label1)], xy_grid[2, .!(label1)], seriestype=:scatter)
+    savefig(p, "decisionboundary_epoch$(epoch).png")
+
     x_t, y_t = first(loader_test);
     y_pred = model(x_t)[end-1:end, :]
     label1 = y_pred[1,:] .> y_pred[2,:]
-    p = plot(x_t[1, label1], x_t[2, label1], seriestype=:scatter)
+    p = plot(x_t[1, label1], x_t[2, label1], title="epoch $(epoch)", seriestype=:scatter)
     plot!(p, x_t[1, .!(label1)], x_t[2, .!(label1)], seriestype=:scatter)
+    savefig(p, "points_epoch$(epoch).png")
+
+    p = plot(y_pred[1, label1], y_pred[2, label1], title="epoch $(epoch)", seriestype=:scatter)
+    plot!(p, y_pred[1, .!(label1)], y_pred[2, .!(label1)], title="epoch $(epoch)", seriestype=:scatter)
+    savefig(p, "cluster_assignment$(epoch).png")
 
     @show epoch, Flux.Losses.binarycrossentropy(1.0 .- y_pred, y_t)
 end
