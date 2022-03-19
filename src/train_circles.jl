bfedf73e2cb24d260985675fa51f16ea100021c4
Ralph Kube <ralph_kube@gmx.net>
2022-03-19 11:33:18 -0700

    More working changes

--- src/train_circles.jl
+++ src/train_circles.jl
@@ -1,5 +1,6 @@
 using Flux
 using Flux.Data: DataLoader
+using CUDA
 using Zygote
 using LinearAlgebra
 using Plots
@@ -9,17 +10,17 @@ using Random
 using dd_clustering
 
 
-hidden_dim = 16
+hidden_dim = 128
 num_classes = 2
 
 r1 = 0.5
-r2 = 1.0
-r3 = 2.0
-num_pts = 32;
+r2 = 3.0
+r3 = 4.0
+num_pts = 16384;
 
-num_epochs = 20
+num_epochs = 5
 
-batch_size = 8;
+batch_size = 256;
 test_split = 0.5;
 
 # Get the dataset
@@ -33,52 +34,42 @@ all_idx = shuffle(1:num_pts);
 train_idx = all_idx[1:num_test];
 test_idx = all_idx[num_test + 1:end];
 
-loader_train = DataLoader((data=X[:, train_idx], label=Y[:, train_idx]), batchsize=batch_size, shuffle=true);
-loader_test = DataLoader((data=X[:, test_idx], label=Y[:, test_idx]), batchsize=batch_size, shuffle=true);
+loader_train = DataLoader((data=X[:, train_idx], label=Y[:, train_idx]), batchsize=batch_size, shuffle=false);
+loader_test = DataLoader((data=X[:, test_idx], label=Y[:, test_idx]), batchsize=batch_size, shuffle=false);
 
-model = Chain(Dense(2, hidden_dim, swish, init=Flux.kaiming_uniform),
-              Dense(hidden_dim, hidden_dim, swish, init=Flux.kaiming_uniform),
-              Dense(hidden_dim, hidden_dim, swish, init=Flux.kaiming_uniform),
+model = Chain(Dense(2, hidden_dim, tanh, init=Flux.kaiming_uniform),
+              Dense(hidden_dim, hidden_dim, tanh, init=Flux.kaiming_uniform),
+              Dense(hidden_dim, hidden_dim, tanh, init=Flux.kaiming_uniform),
               Parallel(vcat, x -> x, Chain(Dense(hidden_dim, num_classes, init=Flux.kaiming_uniform), x -> softmax(x)))) |> gpu;
 ps = Flux.params(model)
 
-opt = ADAM(1e-3)
+opt = ADAM(1e-5)
 
-# epoch=0
-# xy_grid = hcat(repeat(-1.0:0.01:1.0, outer=201), repeat(-1.0:0.01:1.0, inner=201))';
-# y_pred = model(xy_grid)[end-1:end,:];
-# label1 = y_pred[1,:] .> y_pred[2,:];
-# p = plot(xy_grid[1, label1], xy_grid[2, label1], title="epoch $(epoch)", seriestype=:scatter)
-# plot!(p, xy_grid[1, .!(label1)], xy_grid[2, .!(label1)], seriestype=:scatter)
-# savefig(p, "decisionboundary_epoch0.png");
+epoch=0
+xy_grid = hcat(repeat(-1.0:0.01:1.0, outer=201), repeat(-1.0:0.01:1.0, inner=201))' |> gpu;
+y_pred = model(xy_grid)[end-1:end,:];
+label1 = y_pred[1,:] .> y_pred[2,:];
+p = plot(xy_grid[1, label1], xy_grid[2, label1], title="epoch $(epoch)", seriestype=:scatter)
+plot!(p, xy_grid[1, .!(label1)], xy_grid[2, .!(label1)], seriestype=:scatter)
+savefig(p, "decisionboundary_epoch0.png");
 
-# x_t, y_t = first(loader_test);
-# y_pred = model(x_t)[end-1:end, :];
-# label1 = y_pred[1,:] .> y_pred[2,:];
-# p = plot(x_t[1, label1], x_t[2, label1], title="epoch $(epoch)", seriestype=:scatter)
-# plot!(p, x_t[1, .!(label1)], x_t[2, .!(label1)], seriestype=:scatter)
-# savefig(p, "points_epoch$(epoch).png")
+x_t, y_t = first(loader_test);
+y_pred = model(x_t)[end-1:end, :];
+label1 = y_pred[1,:] .> y_pred[2,:];
+p = plot(x_t[1, label1], x_t[2, label1], title="epoch $(epoch)", seriestype=:scatter)
+plot!(p, x_t[1, .!(label1)], x_t[2, .!(label1)], seriestype=:scatter)
+savefig(p, "points_epoch$(epoch).png")
 
-# p = plot(y_pred[1, label1], y_pred[2, label1], title="epoch $(epoch)", seriestype=:scatter)
-# plot!(p, y_pred[1, .!(label1)], y_pred[2, .!(label1)], title="epoch $(epoch)", seriestype=:scatter)
-# savefig(p, "cluster_assignment$(epoch).png")
+p = plot(y_pred[1, label1], y_pred[2, label1], title="epoch $(epoch)", seriestype=:scatter)
+plot!(p, y_pred[1, .!(label1)], y_pred[2, .!(label1)], title="epoch $(epoch)", seriestype=:scatter)
+savefig(p, "cluster_assignment$(epoch).png")
+
+sigma2 = 1.0f0;
 
 for epoch in 1:num_epochs
+    # println("\n\n\n\n\n")
     for (x,y) in loader_train
         size(x)[2] != batch_size && continue
-
-        y_pred = model(x);
-        y_hidden = y_pred[1:end-2, :]       # Output of the last fully-connected layer before the softmax
-        A = y_pred[end-1:end, :]            # A contains the cluster assignments. Compared to the article, A is transposed
-        show(A)
-
-        # See https://github.com/DanielTrosten/mvc/blob/b0a08fc6c75bdb1fae796f82a7cbfb001bf02047/src/lib/kernel.py#L45
-        # Note that Julia's batch dimension is 1 (using 1-based index). PyTorch's batch dimension is 0, counted in 0-based indexing.
-        xyT = y_hidden' * y_hidden
-        x2 = sum(y_hidden.^2, dims=1)
-        distances = x2' .- 2f0 * xyT + repeat(x2, batch_size)
-        sigma2 = 0.15f0 * median(distances)       
-
         loss, back = Zygote.pullback(ps) do 
             y_pred = model(x);
             y_hidden = y_pred[1:end-2, :]       # Output of the last fully-connected layer before the softmax
@@ -88,15 +79,17 @@ for epoch in 1:num_epochs
             # Note that Julia's batch dimension is 1 (using 1-based index). PyTorch's batch dimension is 0, counted in 0-based indexing.
             xyT = y_hidden' * y_hidden
             x2 = sum(y_hidden.^2, dims=1)
-            distances = x2' .- 2xyT + repeat(x2, batch_size)
-            K = exp.(-0.5f0 .* distances .* distances / sigma2)
+            distances_squared = x2' .- 2xyT + repeat(x2, batch_size)
+            Zygote.ignore() do
+                global sigma2 = 0.15f0 * median(distances_squared);
+            end
+            K = exp.(-0.5f0 .* distances_squared / sigma2)
 
             # Calculate the matrix M.
             xyT = A' * CUDA.CuArray([1f0 0f0; 0f0 1f0]);
-            #xyT = A' * I(num_classes);
             x2 = sum(A.^2, dims=1);
             y2 = ones(num_classes)' |> gpu;
-            M = exp.(-x2' .+ 2xyT - repeat(y2, batch_size))'
+            M = exp.(-sqrt.(x2' .- 2xyT + repeat(y2, batch_size) .+ eps(eltype(x2))))'
 
             loss_cs = 0.0f0
             loss_simp = 0.0f0
@@ -106,35 +99,42 @@ for epoch in 1:num_epochs
                     loss_simp += M[i, :]' * K * M[j, :] / sqrt(M[i, :]' * K * M[i, :] * M[j, :]' * K * M[j, :] + eps(eltype(K)))
                 end
             end
-
-
             loss_orth = 2f0 * sum(triu(A' * A, 1)) / (batch_size * (batch_size -1))
 
+            # Zygote.ignore() do
+            #     println("A:")
+            #     display(A)
+            #     println("K:")
+            #     display(K)
+            #     println("M:")
+            #     display(M)
+            #     println("A'*A:")
+            #     display(triu(A'*A))
+            # end
             @show loss_cs, loss_simp, loss_orth, sigma2
             (loss_cs + loss_simp) / num_classes + loss_orth
- 
         end
         grads = back(one(loss))
 
         Flux.update!(opt, ps, grads)
     end
-    # xy_grid = hcat(repeat(-1.0:0.01:1.0, outer=201), repeat(-1.0:0.01:1.0, inner=201))';
-    # y_pred = model(xy_grid)[end-1:end,:]
-    # label1 = y_pred[1,:] .> y_pred[2,:]
-    # p = plot(xy_grid[1, label1], xy_grid[2, label1], title="epoch $(epoch)", seriestype=:scatter)
-    # plot!(p, xy_grid[1, .!(label1)], xy_grid[2, .!(label1)], seriestype=:scatter)
-    # savefig(p, "decisionboundary_epoch$(epoch).png")
+    xy_grid = hcat(repeat(-1.0:0.01:1.0, outer=201), repeat(-1.0:0.01:1.0, inner=201))' |> gpu;
+    y_pred = model(xy_grid)[end-1:end,:];
+    label1 = y_pred[1,:] .> y_pred[2,:];
+    p = plot(xy_grid[1, label1], xy_grid[2, label1], title="epoch $(epoch)", seriestype=:scatter);
+    plot!(p, xy_grid[1, .!(label1)], xy_grid[2, .!(label1)], seriestype=:scatter);
+    savefig(p, "decisionboundary_epoch$(epoch).png");
 
     x_t, y_t = first(loader_test);
-    y_pred = model(x_t)[end-1:end, :]
-    label1 = y_pred[1,:] .> y_pred[2,:]
-    # p = plot(x_t[1, label1], x_t[2, label1], title="epoch $(epoch)", seriestype=:scatter)
-    # plot!(p, x_t[1, .!(label1)], x_t[2, .!(label1)], seriestype=:scatter)
-    # savefig(p, "points_epoch$(epoch).png")
+    y_pred = model(x_t)[end-1:end, :];
+    label1 = y_pred[1,:] .> y_pred[2,:];
+    p = plot(x_t[1, label1], x_t[2, label1], title="epoch $(epoch)", seriestype=:scatter);
+    plot!(p, x_t[1, .!(label1)], x_t[2, .!(label1)], seriestype=:scatter);
+    savefig(p, "points_epoch$(epoch).png");
 
-    # p = plot(y_pred[1, label1], y_pred[2, label1], title="epoch $(epoch)", seriestype=:scatter)
-    # plot!(p, y_pred[1, .!(label1)], y_pred[2, .!(label1)], title="epoch $(epoch)", seriestype=:scatter)
-    # savefig(p, "cluster_assignment$(epoch).png")
+    p = plot(y_pred[1, label1], y_pred[2, label1], title="epoch $(epoch)", seriestype=:scatter);
+    plot!(p, y_pred[1, .!(label1)], y_pred[2, .!(label1)], title="epoch $(epoch)", seriestype=:scatter);
+    savefig(p, "cluster_assignment$(epoch).png");
 
     @show epoch, Flux.Losses.binarycrossentropy(1.0 .- y_pred, y_t)
 end
